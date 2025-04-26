52b7a993afe8b8bbce9d9b218c21b2e2857697f5
Mitesh Godne <129673121+MiteshGodne@users.noreply.github.com>
2025-04-26 21:40:42 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React frontend
+    allow_origins=["https://teach-mate-web-app.vercel.app/", "https://teach-mate-web-app.vercel.app/upload"],  # React frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
